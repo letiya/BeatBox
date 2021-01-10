aa36f22cb33c755afe7591807a6809a1e8bf8893
Belle Lee <letiya66@gmail.com>
2021-01-10 14:24:11 -0800

    Import Assets and get Assets.

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.beatbox;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
+
+public class BeatBox {
+    private static final String TAG = "BeatBox";
+
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+
+    private AssetManager mAssets;
+
+    public BeatBox(Context context) {
+        mAssets = context.getAssets();
+        loadSound();
+    }
+
+    private void loadSound() {
+        String[] soundNames;
+        try {
+            soundNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found " + soundNames.length + " sounds");
+        } catch (IOException e) {
+            Log.e(TAG, "Could not list assets", e);
+            return;
+        }
+    }
+}
