6633fa79b7ad1dd91717d252b83c8ac42faf7de9
Belle Lee <letiya66@gmail.com>
2021-01-09 19:14:22 -0800

    Create BeatBox

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.beatbox;
+
+import androidx.fragment.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
