b8eb2b9fb0e411949819ed17ca67927c8c92aa86
Belle Lee <letiya66@gmail.com>
2021-01-12 23:26:34 -0800

    Create a SoundPool. Load sounds to SoundPool for later use.

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -1,7 +1,10 @@
 package com.bignerdranch.android.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -12,12 +15,16 @@ public class BeatBox {
     private static final String TAG = "BeatBox";
 
     private static final String SOUNDS_FOLDER = "sample_sounds";
+    private static final int MAX_SOUND = 5;
 
     private AssetManager mAssets;
     private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+        // This old constructor is deprecated but needed for compatibility
+        mSoundPool = new SoundPool(MAX_SOUND, AudioManager.STREAM_MUSIC, 0);
         loadSound();
     }
 
@@ -32,10 +39,21 @@ public class BeatBox {
         }
 
         for (String fileName : soundNames) {
-            String assetPath = SOUNDS_FOLDER + "/" + fileName;
-            Sound sound = new Sound(assetPath);
-            mSounds.add(sound);
+            try {
+                String assetPath = SOUNDS_FOLDER + "/" + fileName;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException e) {
+                Log.e(TAG, "Could not load sound " + fileName, e);
+            }
         }
+    }
+
+    private void load(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
     }
 
     public List<Sound> getSounds() {
--- app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -5,6 +5,8 @@ public class Sound {
     private String mAssetPath;
     private String mName;
 
+    private Integer mSoundId;
+
     public Sound(String assetPath) {
         mAssetPath = assetPath;
         String[] components = assetPath.split("/");
@@ -18,5 +20,13 @@ public class Sound {
 
     public String getName() {
         return mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
     }
 }
